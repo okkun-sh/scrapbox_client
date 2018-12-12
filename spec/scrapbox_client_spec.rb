c0972ae085358585ac8fa41d9c20cdcd67d9799e
okkun_sh <okkun.sh@gmail.com>
2018-12-12 23:34:42 +0900

    tweak test

--- spec/scrapbox_client_spec.rb
+++ spec/scrapbox_client_spec.rb
@@ -1,11 +1,12 @@
 require "spec_helper"
 
-RSpec.describe 'module test' do
-  let(:scrapbox_client) { ScrapboxClient::Client.new(project_name: 'apisample') }
+RSpec.describe "module test" do
+#  let(:scrapbox_client) { ScrapboxClient::Client.new(project_name: "apisampl") }
 
-  describe '#fetch_page_list' do
-    it "fetch_page_list" do
-      scrapbox_client.fetch_page_list
+  describe "#fetch_page_list" do
+    subject(:success_fetch_page_list) { ScrapboxClient::Client.new(project_name: "apisample").fetch_page_list }
+    it "request success" do
+      expect(success_fetch_page_list.code).to eq "200"
     end
   end
 end
