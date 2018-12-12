fddb1aa8e5897a293e00119b8aa0e97af8ac4e1d
okkun_sh <okkun.sh@gmail.com>
2018-12-13 00:11:59 +0900

    added api

--- lib/scrapbox_client.rb
+++ lib/scrapbox_client.rb
@@ -10,9 +10,9 @@ module ScrapboxClient
     BASE_URI_V1 = "https://scrapbox.io"
     URI_RESOURCE_MAP = {
       page_list: "/api/pages/%{project_name}?skip=%{skip}&limit=%{limit}",
-      page: "/api/pages/%{project_name}/%{project_title}",
-      page_body: "/api/pages/%{project_name}/%{project_title}/text",
-      page_image: "/api/pages/%{project_name}/%{project_title}/icon",
+      page: "/api/pages/%{project_name}/%{page_title}",
+      page_body: "/api/pages/%{project_name}/%{page_title}/text",
+      page_image: "/api/pages/%{project_name}/%{page_title}/icon",
     }
 
     def initialize(project_name:)
@@ -30,6 +30,39 @@ module ScrapboxClient
       return res
     end
 
+    def fetch_page(page_title:)
+      return unless page_title
+      params = {
+        project_name: project_name,
+        page_title: page_title,
+      }
+      res = request_api(resource: URI_RESOURCE_MAP[:page], params: params)
+
+      return res
+    end
+
+    def fetch_page_body(page_title:)
+      return unless page_title
+      params = {
+        project_name: project_name,
+        page_title: page_title,
+      }
+      res = request_api(resource: URI_RESOURCE_MAP[:page_body], params: params)
+
+      return res
+    end
+
+    def fetch_page_image(page_title:)
+      return unless page_title
+      params = {
+        project_name: project_name,
+        page_title: page_title,
+      }
+      res = request_api(resource: URI_RESOURCE_MAP[:page_image], params: params)
+
+      return res
+    end
+
     private
     def request_api(resource:, params:)
       uri = URI.parse(BASE_URI_V1 + sprintf(resource, params))
@@ -38,7 +71,7 @@ module ScrapboxClient
         http.read_timeout = 10
         http.get(uri.request_uri)
       end
-      unless res.code == '200'
+      unless res.code == '200' || res.code == '302'
         raise "request failed with status code " + res.code
       end
 
--- spec/scrapbox_client_spec.rb
+++ spec/scrapbox_client_spec.rb
@@ -9,4 +9,25 @@ RSpec.describe "module test" do
       expect(success_fetch_page_list.code).to eq "200"
     end
   end
+
+  describe "#page" do
+    subject(:success_fetch_page) { ScrapboxClient::Client.new(project_name: "apisample").fetch_page(page_title: "api_sample_title") }
+    it "request success" do
+      expect(success_fetch_page.code).to eq "200"
+    end
+  end
+
+  describe "#page_body" do
+    subject(:success_fetch_page_body) { ScrapboxClient::Client.new(project_name: "apisample").fetch_page_body(page_title: "api_sample_title") }
+    it "request success" do
+      expect(success_fetch_page_body.code).to eq "200"
+    end
+  end
+
+  describe "#page_image" do
+    subject(:success_fetch_page_image) { ScrapboxClient::Client.new(project_name: "apisample").fetch_page_image(page_title: "api_sample_title") }
+    it "request success" do
+      expect(success_fetch_page_image.code).to eq "302"
+    end
+  end
 end
